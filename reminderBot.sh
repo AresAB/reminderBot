349812ede2d082e9f61da89ce5e4072b911c08d5
AresAB <ares.kittell@gmail.com>
2024-08-01 12:50:07 -0700

    removed test.sh from the hub

--- reminderBot.sh
+++ reminderBot.sh
@@ -3,20 +3,15 @@
 repeat="y"
 
 while [ $repeat = "y" ]; do
+    for i in $(seq 1 $1); do
+        sleep 1
+    done
 
-for i in $(seq 1 $1); do
-sleep 1
-done
+    notepad.exe 20-20-20.txt
+    repeat="tmp"
 
-notepad.exe 20-20-20.txt
-
-repeat="tmp"
-
-while [ $repeat != "y" ] && [ $repeat != "n" ]; do
-
-echo "Repeat? [y/n]"
-read repeat
-
-done
-
+    while [ $repeat != "y" ] && [ $repeat != "n" ]; do
+        echo "Repeat? [y/n]"
+        read repeat
+    done
 done
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-five=5
-three=3
-var=$(( $five - $three ))
-
-if [ $var -eq 2 ]; then
-echo $SECONDS
-fi
